50ab900fe37f73a55d6d82cf4479d66fd7e29ab8
Rohit-Pandit <159272518+Rohit-Pandit@users.noreply.github.com>
2025-08-06 16:08:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+	const ele = document.getElementById("para");
+	ele.textContent = "Entered Metaverse"
+})
